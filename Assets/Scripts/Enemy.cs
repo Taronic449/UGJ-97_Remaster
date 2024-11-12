e2b54edae0413801cb8b2649cd26cdb8f08310a4
ArcticPingu <finnianbecker@gmail.com>
2024-11-12 22:28:01 +0100

    EnemyAI

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -11,21 +11,60 @@ public class Enemy : MonoBehaviour
     private NavMeshAgent agent;
 
     public Vector2 startoff;
+    public bool reingage;
+    public BrainType brainType;
+    public float smartTimer = 15f;
+
+
+    public enum BrainType
+    {
+        agressive,
+        passive,
+        smart,
+        speed
+    }
 
     void Start()
     {
+        brainType = (BrainType)Random.Range(0,4);
+
         player = FindAnyObjectByType<PlayerController>().transform;
         ani = GetComponent<Animator>();
         sprite = GetComponent<SpriteRenderer>();
         agent = GetComponent<NavMeshAgent>();
 
-        startoff = Random.insideUnitCircle * 1;
+        StartBrainEvent();
+        
     }
 
 
     void FixedUpdate()
     {
-        GetComponent<NavMeshAgent>().SetDestination(player.transform.position + (Vector3) startoff);
+        if(brainType == BrainType.smart)
+        {
+            if(smartTimer <= 0)
+            {
+               
+                GetComponent<NavMeshAgent>().SetDestination(player.transform.position + (Vector3) startoff);
+            }
+            else
+            {
+                if(transform.position.x > player.transform.position.x)
+                {
+                    startoff = new Vector2(0.3f,0);
+                }
+                else
+                {
+                    startoff = new Vector2(-0.3f,0);
+                }
+                
+                GetComponent<NavMeshAgent>().SetDestination(player.transform.position + (Vector3) startoff * 5);
+            }
+        }
+        else
+        {
+            GetComponent<NavMeshAgent>().SetDestination(player.transform.position + (Vector3) startoff);
+        }
 
         sprite.flipX = player.position.x - transform.position.x > 0;
 
@@ -33,11 +72,127 @@ public class Enemy : MonoBehaviour
         {
             timer = 3;
             ani.SetBool("attack", true);
+            reingage = false;   
+            BrainEvent();
 
             agent.isStopped = true;
         }
 
+        if(!reingage && timer < 0)
+        {
+            reingage = true;
+
+            BrainReturnEvent();
+        }
+        
+        smartTimer -= Time.deltaTime;
         timer -= Time.deltaTime;
+    }
+
+    public void BrainReturnEvent()
+    {
+        switch (brainType)
+        {
+            case BrainType.agressive:
+            return;
+
+            case BrainType.passive:
+
+                if(transform.position.x > player.transform.position.x)
+                {
+                    startoff = new Vector2(0.3f,0);
+
+                }
+                else
+                {
+                    startoff = new Vector2(-0.3f,0);
+                }
+
+            return;
+
+            case BrainType.smart:
+
+                if(transform.position.x > player.transform.position.x)
+                {
+                    startoff = new Vector2(0.3f,0);
+
+                }
+                else
+                {
+                    startoff = new Vector2(-0.3f,0);
+                }
+
+            return;
+
+            case BrainType.speed:
+                startoff = (transform.position - player.transform.position).normalized * 0.4f;
+            return;
+
+        }
+    }
+
+    public void BrainEvent()
+    {
+        switch (brainType)
+        {
+            case BrainType.agressive:
+            return;
+
+            case BrainType.passive:
+
+                startoff = (transform.position - player.transform.position).normalized;
+
+            return;
+
+            case BrainType.smart:
+
+                startoff = (transform.position - player.transform.position).normalized * 3;
+
+            return;
+
+            case BrainType.speed:
+                timer = 1.5f;
+                startoff = (transform.position - player.transform.position).normalized * 3;
+            return;
+
+        }
+    }
+
+    public void StartBrainEvent()
+    {
+        switch (brainType)
+        {
+            case BrainType.agressive:
+                startoff = Random.insideUnitCircle * 1;
+            return;
+
+            case BrainType.passive:
+
+                if(transform.position.x > player.transform.position.x)
+                {
+                    startoff = new Vector2(0.3f,0);
+
+                }
+                else
+                {
+                    startoff = new Vector2(-0.3f,0);
+                }
+
+            return;
+
+            case BrainType.smart:
+
+                GetComponent<NavMeshAgent>().speed = 1.8f;
+                GetComponent<Animator>().speed = 1.4f;
+
+            return;
+
+            case BrainType.speed:
+                GetComponent<NavMeshAgent>().speed = 1.5f;
+                GetComponent<Animator>().speed = 1.5f;
+            return;
+
+        }
     }
 
     public void Death()
